ac9dc6c540206c0c1c97be1576931f9df85932aa
jk09222 <jk09222@jbnu.ac.kr>
2025-04-13 19:50:47 +0900

    homework

--- Homework/FirstHomework/FirstHomework/array_max.cpp
+++ Homework/FirstHomework/FirstHomework/array_max.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-// 최댓값을 구하는 함수
+
 int findMax(int arr[], int size) {
-    int max = arr[0]; // 첫 번째 값을 기준으로 초기화
+    int max = arr[0]; 
 
     for (int i = 1; i < size; i++) {
         if (arr[i] > max)
@@ -13,11 +13,11 @@ int findMax(int arr[], int size) {
 }
 
 int main() {
-    // 배열 선언 (직접 값 지정)
+    
     int arr[] = { 3, 7, 2, 9, 10, 5 };
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    // 최댓값 찾기 및 출력
+    
     int result = findMax(arr, size);
     printf("최댓값: %d\n", result);
 
