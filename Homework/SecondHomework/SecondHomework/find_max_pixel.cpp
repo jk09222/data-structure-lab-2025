01302e7701b1e70a5cedc4db3f0a9b0082f85fcf
jk09222 <jk09222@jbnu.ac.kr>
2025-05-19 10:03:16 +0900

    Lec09

--- Homework/SecondHomework/SecondHomework/find_max_pixel.cpp
+++ Homework/SecondHomework/SecondHomework/find_max_pixel.cpp
@@ -7,29 +7,30 @@
 //} 
 
 int main() {
-	cv::Mat img = cv::imread("Lenna.png", cv::IMREAD_GRAYSCALE);
-	cv::imshow("lenna window", img);
-	cv::waitKey(0);
+    cv::Mat img = cv::imread("Lenna.png", cv::IMREAD_GRAYSCALE);
+    cv::imshow("lenna window", img);
+    cv::waitKey(0);
 
+    // 이미지 로딩 확인
     if (img.empty()) {
         printf("이미지를 불러올 수 없습니다.\n");
         return -1;
     }
 
-    unsigned char max = 0;
+    // 최대 밝기값 초기화
+    unsigned char maxVal = 0;
 
-    
+    // 전체 픽셀 순회하며 최대 밝기값 찾기
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
             unsigned char pixel = img.at<unsigned char>(i, j);
-            if (pixel > max)
-                max = pixel;
+            if (pixel > maxVal)
+                maxVal = pixel;
         }
     }
 
-    
-    printf("최대 밝기값: %d\n", max);
+    // 최대 밝기 출력
+    printf("최대 밝기값: %d\n", maxVal);
 
     return 0;
-
 }
